e82b7a8761056bd0275f3bd2092a307922adcf54
Masha Lifshin <mlifshin@gmail.com>
2013-04-04 18:22:20 -0700

    Add the rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ferrisamericus::Application.config.secret_token = '267fee4957cb9b90615b909e2fae138197131babd90df28adabd33f0c52283149253dab5f5ff6cb6d418cb83571fd70c11f75c5e038a47ec5b3523c239969327'
