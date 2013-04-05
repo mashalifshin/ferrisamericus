f2cae59562e12d44b29dc2c87bfa331f5fb9f959
Masha Lifshin <mlifshin@gmail.com>
2013-04-04 20:16:17 -0700

    Make new live and name attributes accessible.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,8 @@
 class Piece < ActiveRecord::Base
   has_many :piece_images
 
-  attr_accessible :date, :description, :text, :title, :piece_image_ids, :piece_images_attributes
+  attr_accessible :date, :description, :text, :title, :piece_image_ids, :piece_images_attributes,
+                  :live
 
   accepts_nested_attributes_for :piece_images, :allow_destroy => true
 end
--- app/models/piece_image.rb
+++ app/models/piece_image.rb
@@ -1,7 +1,7 @@
 class PieceImage < ActiveRecord::Base
   belongs_to :piece
 
-  attr_accessible :image, :image_cache, :remove_image, :piece_id
+  attr_accessible :name, :image, :image_cache, :remove_image, :piece_id
 
   mount_uploader :image, PieceImageUploader
 end
