f694da0b50fd30d7e766336a4c0fe9b377f02585
Masha Lifshin <mlifshin@gmail.com>
2013-04-25 16:21:51 -0700

    Use image order for piece images.

--- app/models/piece_image.rb
+++ app/models/piece_image.rb
@@ -1,7 +1,9 @@
 class PieceImage < ActiveRecord::Base
   belongs_to :piece
 
-  attr_accessible :name, :image, :image_cache, :remove_image, :piece_id
+  attr_accessible :name, :image, :image_order, :image_cache, :remove_image, :piece_id
 
   mount_uploader :image, PieceImageUploader
+
+  scope :ordered, :order => 'image_order ASC'
 end
